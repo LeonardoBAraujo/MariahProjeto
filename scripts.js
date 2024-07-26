1b9fcbc5136710188c378987be7da56be44f63e9
Leonardo Araújo <leonardo.web87@gmail.com>
2024-07-26 19:42:02 -0300

    inserção de novas imagens,alteração no JS

--- scripts.js
+++ scripts.js
@@ -1,14 +1,34 @@
 // document = ir no HTML
 // querySelector = busca o que eu quiser que ele busque
 
-let formulario = document.querySelector(".formulario")
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.getElementById('curtir');
-
-    button.addEventListener('click', () => {
-        button.classList.add('clicked');
+document.addEventListener('DOMContentLoaded', () => {
+    const album = document.querySelector('.album');
+    let currentPage = 0;
+    
+    window.addEventListener('wheel', (event) => {
+        if (event.deltaY > 0) {
+            nextPage();
+        } else {
+            previousPage();
+        }
     });
-});
 
+    function nextPage() {
+        if (currentPage < album.children.length - 1) {
+            currentPage++;
+            updateAlbum();
+        }
+    }
+
+    function previousPage() {
+        if (currentPage > 0) {
+            currentPage--;
+            updateAlbum();
+        }
+    }
+
+    function updateAlbum() {
+        const height = album.children[0].clientHeight + 20; // Adding margin
+        album.style.transform = `translateY(${-currentPage * height}px)`;
+    }
+});
